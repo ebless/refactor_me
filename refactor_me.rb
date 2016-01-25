f4d76d17b8d886b2e8f54992f7f6916eca40df0d
Ned Bless <ebless@lsoc.org>
2016-01-25 08:44:11 -0600

    General code refactoring

--- refactor_me.rb
+++ refactor_me.rb
@@ -1,20 +1,31 @@
-puts "enter your First Name"
+puts "Enter your first name."
 first_name = gets.chomp
-puts "Enter your Last name"
+
+puts "Enter your last name."
 last_name = gets.chomp
-puts "enter your age"
-age = gets
+
+puts "Enter your age."
+age = gets.chomp
+age = age.to_i
+
 if age <= 15
-puts "Enjoy life while you're young kid."
-elsif age = 16
-puts " What is your favorite car"
-car = gets.chomp
-puts "better get a job, a #{car.downcase} doesn't buy itself"
-elseif age > 16
-puts "What do you want to be when you grow up?"
-dream = "gets.chomp"
-puts "I'll see you in 5 years, " + "first_name.capital" + last_name.capital. + "The #{dream}.".upcase
-elsif > 18
-"Woo! #{first_name.capitalize} is an adult!"
-"Watch out world!"
+	puts "Enjoy life while you're young, kid."
+	puts ''
+elsif age >= 16
+	puts "What is your favorite car?"
+	car = gets.chomp
+	puts "Better get a job, a #{car.capitalize} doesn't buy itself!"
+	puts ''
+end
+
+if age > 16
+	puts "What do you want to be when you grow up?"
+	dream = gets.chomp
+	puts "I'll see you in 5 years, " + first_name.capitalize + ' ' + last_name.capitalize + ' ' + "the #{dream.downcase}."
+	puts ''
+end
+
+if age >= 18
+	puts "Woo! #{first_name.capitalize} is an adult!"
+	puts "Watch out world!"
 end
